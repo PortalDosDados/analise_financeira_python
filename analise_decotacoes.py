732fe518200f172718ec2a8f4e3b91dceb19931b
Dione Nascimento <90805395+PortalDosDados@users.noreply.github.com>
2025-10-26 14:39:07 -0300

    Finish

--- analise_decotacoes.py
+++ analise_decotacoes.py
@@ -14,7 +14,7 @@ end_date = ('2015-12-31')
 df =yf.download(tickers, start=start_date, end=end_date)
 
 # Exibindo as 5 primeiras linhas
-print(df.head())
+#print(df.head())
 
 # Cria uma lista de DataFrames individuais, um para cada ticker
 dfs = []
@@ -31,4 +31,28 @@ df_longo = pd.concat(dfs).reset_index()
 df_longo = df_longo[['Date', 'Ticker', 'Close','High', 'Low','Open', 'Volume']]
 
 # Exibindo colunas ajustadas
-print(df_longo.head())
+#print(df_longo.head())
+
+
+# Salvando em Excel
+df_longo.to_excel('cotações.xlsx', index=False)
+
+# Loop para obter dividendos de todos os tickers
+df_dividendos = pd.concat(
+    [pd.DataFrame({
+        'Date': yf.Ticker(t).dividends.index,
+        'Dividend': yf.Ticker(t).dividends.values,
+        'Ticker': t
+    }) for t in tickers]
+).reset_index(drop=True)
+
+#print(df_dividendos.head())
+
+# Remove o fuso horário da coluna Date
+df_dividendos['Date'] = df_dividendos['Date'].dt.tz_localize(None)
+
+# Arredonda a coluna Dividend para 2 casas decimais
+df_dividendos['Dividend'] = df_dividendos['Dividend'].round(2)
+
+# Salvando em Excel
+df_dividendos.to_excel('dividendos.xlsx',index=False)
